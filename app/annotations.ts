9c8c8f5dfc4a17307987d740f1130e466b8c9c88
alvinchiew <jingjie95@gmail.com>
2023-07-26 02:13:00 +0800

    done learning annotations

--- app/annotations.ts
+++ app/annotations.ts
@@ -5,7 +5,7 @@ const truths: boolean[] = [true, false];
 const cars: string[][] = [['toyota'], ['honda']];
 const dates: (Date | string)[] =  [new Date()];
 dates.push('2023-01-01');
-console.log(dates);
+// console.log(dates);
 
 // tuple
 // useful to handle csv
@@ -21,19 +21,62 @@ const carSpecs = {
 }
 
 // class
+class Vehicle {
+  constructor(public color: string) {}
+
+  // method 
+  protected honk(): string {
+    return 'chuu chuu'
+  }
+
+  private drive(): string {
+    return 'vroom'
+  }
+}
+
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string){ super(color)}
+
+  public honk(): string {
+    return 'vroom'
+  }
+
+  private rev(): string{
+    return `reving...`
+  }
+
+  start_engine(): string {
+    return this.rev()
+  }
+}
+
+const train = new Vehicle('White');
+const civic = new Car(4, 'White');
+// console.log(train.honk());
+// console.log(train.drive());
+console.log(civic.honk());
+// console.log(civic.drive());
+console.log(civic.start_engine());
+
 class Color {
   r: number;
   g: number;
   b: number;
 
-  constructor(r:number,g:number,b:number=0) {
+  constructor(r:number,g:number,b:number=100) {
     this.r = r;
     this.g = g;
     this.b = b;
   }
+
+  // method 
+  print_color(): string {
+    return `${this.r} ${this.g} ${this.b}`
+  }
 }
 
 const color: Color = new Color(0,0);
+// console.log(color.print_color());
 
 // literal
 const coords: {x: number; y: number} = {
@@ -44,10 +87,10 @@ const coords: {x: number; y: number} = {
 // function
 // - arrow function
 const log: (i: number) => void = (i: number) => {
-  console.log(i);
+  // console.log(i);
 }
 const log2 = (i: number): void => {
-  console.log(i);
+  // console.log(i);
 }
 // - named function
 function add(x: number, y: number): number {
@@ -63,9 +106,9 @@ const forecast_today = {
   weather: 'sunny'
 }
 const logWeather = (forecast: {date: Date, weather: string}): void => {
-  console.log(forecast.date, forecast.weather); 
+  // console.log(forecast.date, forecast.weather); 
 };
-console.log(logWeather(forecast_today));
+// console.log(logWeather(forecast_today));
 
 const profile = {
   person_name: 'Alvin',
@@ -74,8 +117,8 @@ const profile = {
     x: 0,
     y: 1 
   },
-  setAge(age: number): void {
-    this.age = age;
+  setAge(person_age: number): void {
+    this.person_age = person_age;
   }
 }
 const {person_name, person_age}: {person_name: string; person_age:number} = profile;
@@ -87,13 +130,13 @@ const {coords: {x, y}}:{coords: {x:number; y:number}} = profile;
 // - Function that returns `any` type
 const json = '{"x":0,"y":1}'
 const coords_json: {x: number; y: number} = JSON.parse(json)
-console.log(coords_json);
+// console.log(coords_json);
 // - Variable declaration without initialization
 let found_word: boolean;
 let words = ['red', 'green', 'blue']
 for (let i = 0; i < words.length; i++) {
   found_word = words[i] === 'green' ? true : false
-  console.log(found_word)
+  // console.log(found_word)
 }
 // - Variable with unfixed type (tho it's bad practice)
 const numbers = [-10, -1, 9, -22]
@@ -101,6 +144,34 @@ let numberAboveZero: boolean | number = false;
 
 for (let i = 0; i < numbers.length; i++) {
   numberAboveZero = numbers[i] > 0 ? numbers[i] : false
-  console.log(numberAboveZero);
+  // console.log(numberAboveZero);
 }
 
+// Interface
+interface Reportable {
+  summary(): string;
+}
+
+function report_summary(item: Reportable){
+  console.log(item.summary());
+}
+
+const Civic = {
+  name: 'civic',
+  year: 2000,
+  is_broken: false,
+  summary(): string {
+    return `this is ${this.name}. It is made in ${this.year} and is it broken? ${this.is_broken}`
+  }
+}
+
+const Coke = {
+  name: 'coke',
+  is_carbonated: true,
+  summary(): string {
+    return `this is ${this.name} and is it carbonated? ${this.is_carbonated}`
+  }
+}
+
+// report_summary(Civic);
+// report_summary(Coke);
