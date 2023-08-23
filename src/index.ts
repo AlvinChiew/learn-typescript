7863ac9c2d1369399e93eba50ec0e643341a077f
alvinchiew <jingjie95@gmail.com>
2023-08-23 23:27:22 +0800

    refactored file reader

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import fs from 'fs';
+import { MatchReader } from "./MatchReader";
+import { MatchResult } from "./MatchResult";
 
-const matches = fs.readFileSync('football.csv', { encoding: 'utf8' })
-  .split('\n')
-  .map((row: string):string[] => { return row.split(',')});
+const reader = new MatchReader('football.csv');
+reader.read();
+console.log(reader.data[0]);
 
 let manUnitedWins = 0
 
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++
   }
 }
