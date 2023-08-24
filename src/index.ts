f9e713bb21ee883e4502debfa3f6cf1657f4e50c
alvinchiew <jingjie95@gmail.com>
2023-08-24 23:07:57 +0800

    add Summary with deligated analyzer & reporter

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
 import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
+import { Summary } from "./Summary";
+import { WinsAnalyzer } from "./analyzers/WinAnalysis";
+import { ConsoleReport } from "./reporters/ConsoleReport";
+import { HtmlReport } from "./reporters/HtmlReport";
 
 const csvFilderReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFilderReader);
 matchReader.load();
 
-console.log(matchReader.matches);
-
-let manUnitedWins = 0
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++
-  }
-}
-
-console.log(manUnitedWins)
+console.log(matchReader.matches[0]);
+const summary = new Summary(new WinsAnalyzer('Man United'), new HtmlReport())
+summary.buildAndPrintReport(matchReader.matches); 
